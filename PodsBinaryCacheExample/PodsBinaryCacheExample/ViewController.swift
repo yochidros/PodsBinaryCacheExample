3f471883086f466a65581e32c299dae3b4ccee5a
yochidros <mm9.movement.trb@gmail.com>
2020-09-04 09:26:23 +0900

    init: project.

---
+++ PodsBinaryCacheExample/PodsBinaryCacheExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PodsBinaryCacheExample
+//
+//  Created by miyazawa yoshiki on 2020/09/04.
+//  Copyright © 2020 yochidros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
